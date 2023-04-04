7ee93eb97391ecca28445ef937e2547cbd52bc6b
smayr <mayr97.sm@gmx.at>
2023-04-04 13:33:14 +0200

    uncommented code

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import "os"
 
 func main() {
-	/*a := App{}
+	a := App{}
 	a.Initialize(
 		os.Getenv("APP_DB_USERNAME"),
 		os.Getenv("APP_DB_PASSWORD"),
 		os.Getenv("APP_DB_NAME"))
 
-	a.Run(":8010")*/
-	println(os.Getenv("APP_DB_USERNAME"), os.Getenv("APP_DB_PASSWORD"), os.Getenv("APP_DB_NAME"))
+	a.Run(":8010")
+	//println(os.Getenv("APP_DB_USERNAME"), os.Getenv("APP_DB_PASSWORD"), os.Getenv("APP_DB_NAME"))
 }
